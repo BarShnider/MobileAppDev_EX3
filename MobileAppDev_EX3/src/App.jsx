835d51d74e2303ade8f675d43c8b72473d3c6932
Bar Shnider <Barshnider@gmail.com>
2024-02-12 22:14:59 +0200

    Update App.jsx

--- MobileAppDev_EX3/src/App.jsx
+++ MobileAppDev_EX3/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Login from "./Components/Login"
 import Profile from "./Components/Profile"
 import Register from "./Components/Register"
@@ -6,11 +6,24 @@ import Register from "./Components/Register"
 
 // https://colorhunt.co/palette/c4dfdfd2e9e9e3f4f4f8f6f4
 function App() {
-  const [user,setUsers] = useState([]) // connect to use effect 
+  const [users,setUsers] = useState([]) // connect to use effect 
+
+
+  useEffect(function(){
+    console.log("SET")
+    localStorage.setItem("users", JSON.stringify(users))
+  },[users])
+
+  useEffect(function(){
+    console.log("GET")
+    setUsers(JSON.parse(localStorage.getItem("users")))
+  },[])
+
+  
   return (
     <>
-    {/* <Register />
-    <Login /> */}
+    <Register />
+    <Login />
     <Profile />
     </>
   )
